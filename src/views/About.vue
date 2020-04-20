4458a3178dd375dc7deb2cdafa70d4780da0bdb3
sullay <1181518458@qq.com>
2020-04-20 17:07:27 +0800

    init

---
+++ src/views/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>{{name.name}}</h1>
+    <h1>{{count}}</h1>
+    <h1>{{computedCount}}</h1>
+    <button @click="increamt">+</button>
+  </div>
+</template>
+
+<script>
+import { reactive, ref, computed, onMounted, watch } from 'vue'
+export default {
+  setup () {
+    const name = reactive({
+      name: 'hello xxx'
+    })
+    const count = ref(0)
+    const increamt = () => {
+      count.value++
+    }
+    const computedCount = computed(() => count.value * 100)
+    onMounted(() => {
+      console.log(name.name)
+    })
+    watch(count, (newVal, oldVal) => {
+      console.log(newVal, oldVal)
+    })
+    return { name, count, increamt, computedCount }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld :msg='msg' @update:msg="updateMsg"/>
+    <h1>{{x}}    {{y}}</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import { useMousePosition } from '../hook/page.js'
+import { ref } from 'vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  setup () {
+    const { x, y } = useMousePosition()
+    const msg = ref('Welcome to Your Vue.js App')
+    const updateMsg = (e) => {
+      msg.value = e
+    }
+    return { msg, updateMsg, x, y }
+  }
+}
+</script>
